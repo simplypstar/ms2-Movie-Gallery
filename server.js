eb183fb63a9b6e09547989201e4402826597bdd9
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-26 15:47:30 -0400

    get rid of unused code

--- server.js
+++ server.js
@@ -5,25 +5,6 @@ const methodOverride = require("method-override")
 const app = express()
 const mongoose = require("mongoose")
 
-// DB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('DB connected'))
-    .catch(err => console.error(err));
-
-// async function dbconnection() {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     console.log("DB connected")
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
-
-// dbconnection()
-
 // middleware
 app.set("views", __dirname + "/views")
 app.set("view engine", "jsx")
@@ -44,6 +25,11 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
   res.render("Error404")
 })
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT
 
