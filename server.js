180c60676ccfc0613694b26adf9d5c849c881f6a
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-26 14:17:49 -0400

    update mongo connection

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get('*', (req, res) => {
 })
 
 // DB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = process.env.MONGO_URI
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
