fb4012ec8e79fc1b66efaca5fd71439a604349e4
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-26 15:02:41 -0400

    adjust db connection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ async function dbconnection() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+    //   useUnifiedTopology: true,
     })
     console.log("DB connected")
   } catch (err) {
