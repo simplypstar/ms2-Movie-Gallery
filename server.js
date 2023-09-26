1d5a103f27d496f1d3d9a8b63800696df4892976
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-26 14:46:38 -0400

    change db connect to async/await

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
 // modules and globals
-require ('dotenv').config()
-const express = require('express')
-const methodOverride= require('method-override')
+require("dotenv").config()
+const express = require("express")
+const methodOverride = require("method-override")
 const app = express()
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
+
+async function dbconnection() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log("DB connected")
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+dbconnection()
 
 // middleware
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set("views", __dirname + "/views")
+app.set("view engine", "jsx")
+app.engine("jsx", require("express-react-views").createEngine())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
+app.use(express.static("public"))
+app.use(methodOverride("_method"))
 
 // controllers
-app.use('/movies', require('./controllers/movie'))
+app.use("/movies", require("./controllers/movie"))
 
 // routes
-app.get('/', (req, res) => {
-    res.render('home')
+app.get("/", (req, res) => {
+  res.render("home")
 })
 
-app.get('/movies', (req, res) => {
-    res.redirect('/movies')
+app.get("*", (req, res) => {
+  res.render("Error404")
 })
 
-app.get('*', (req, res) => {
-    res.render('Error404')
-})
-
-// DB connection
-const uri = process.env.MONGO_URI
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('DB connected'))
-    .catch(err => console.error(err));
+// // DB connection
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('DB connected'))
+//     .catch(err => console.error(err));
 
 const PORT = process.env.PORT
 
