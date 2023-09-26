fdc2fe04c1205fd851f2838d4ab3c368139d8cdd
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-26 15:32:21 -0400

    change back

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const mongoose = require("mongoose")
 
 // DB connection
-mongoose.connect("mongodb+srv://mernstack:mernstack@cluster0.ownjxhk.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
